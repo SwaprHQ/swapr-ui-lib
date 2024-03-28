e9b37410044830e5db5e1c2b22dd56f378fbeaaf
Diogo Ferreira <diogomf@hey.com>
2024-03-28 01:28:04 +0000

    - add input component - install @radix-ui/react-label

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./Button";
 export * from "./ChipButton";
 export * from "./Icon";
+export * from "./Input";
 export * from "./Tabs";
 export * from "./Toast";
 export * from "./ToggleGroup";
