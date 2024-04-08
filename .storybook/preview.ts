5065c3bb4129276362f9355923ce0e78c139eb6f
Rorry <rodrigojali@gmail.com>
2024-04-08 18:15:05 -0300

    chore(SWUI-16): add background controls to Storybook parameters

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,26 @@ import "../src/global.css";
 
 const preview: Preview = {
   parameters: {
+    /**
+     * @see https://storybook.js.org/docs/essentials/backgrounds
+     */
+    backgrounds: {
+      default: "Light",
+      values: [
+        {
+          name: "Light",
+          value: "#F3F6F4",
+        },
+        {
+          name: "Gray",
+          value: "#E5E4E2",
+        },
+        {
+          name: "Dark",
+          value: "#36454F",
+        },
+      ],
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
