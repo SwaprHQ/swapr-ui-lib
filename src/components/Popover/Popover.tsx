e785836d688dcf0340dd46925864e11dc52e8a99
Berteotti <berteotti2@gmail.com>
2024-04-30 13:05:51 +0100

    fix: popver bg color

--- src/components/Popover/Popover.tsx
+++ src/components/Popover/Popover.tsx
@@ -29,7 +29,7 @@ const PopoverContent = React.forwardRef<
         align={align}
         sideOffset={sideOffset}
         className={twMerge(
-          "bg-outline-white border border-outline-base-em min-w-32 p-4 rounded-16 z-50",
+          "bg-surface-surface-0 border border-outline-base-em min-w-32 p-4 rounded-16 z-50",
           "data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95",
           "data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95",
           "data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
