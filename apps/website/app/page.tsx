5a6eb72ef3043a3767fe7bea2cf99102cb2e0a72
ElRodrigote <rodrigo.ali@nuant.com>
2024-05-08 15:46:53 +0200

    infra: bring website app to the monorepo

---
+++ apps/website/app/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "swapr-ui";
+
+export default function Home() {
+  return (
+    <main className="bg-surface-neutral-alt-white bg-[url('/lines.svg')] h-screen bg-no-repeat bg-cover flex justify-center">
+      <div className="pt-48 space-y-6 text-center">
+        <h1 className="text-[58px] leading-[64px] font-bold">
+          Build dapps fast
+        </h1>
+        <p className="text-lg text-text-med-em">
+          A set of components made to help you ship web3 apps faster than ever.
+        </p>
+        <Link href="/ui" className="block">
+          <Button onClick={() => console.log("hey")} className="mx-auto">
+            Explore components
+          </Button>
+        </Link>
+      </div>
+    </main>
+  );
+}
---
+++ apps/website/next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  distDir: "dist",
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
