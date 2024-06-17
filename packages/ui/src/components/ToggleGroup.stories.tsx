d8cf32402b404d1cb2c5353f9ca4ed0cdbb88ce3
Diogo Ferreira <diogomf@hey.com>
2024-06-17 19:21:38 +0100

    Add ToggleGroup to storybook

---
+++ packages/ui/src/components/ToggleGroup.stories.tsx
@@ -0,0 +1,39 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { ToggleGroup, ToggleGroupOption } from "./ToggleGroup"; // Corrected spelling here
+import { useState } from "react";
+
+const meta: Meta<typeof ToggleGroup> = {
+  title: "Swapr-ui/ToggleGroup", // Corrected spelling here
+  component: ToggleGroup,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    className: { control: "text" },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const ToggleGroupComponent: Story = {
+  render: args => {
+    const [selectedValue, setSelectedValue] = useState("business");
+
+    return (
+      <ToggleGroup onChange={setSelectedValue} value={selectedValue} {...args}>
+        {["all", "business", "international", "politics"].map(category => (
+          <ToggleGroupOption
+            key={category}
+            value={category}
+            className="capitalize font-bold"
+          >
+            {category}
+          </ToggleGroupOption>
+        ))}
+      </ToggleGroup>
+    );
+  },
+};
