c0642b666755fe1d27e86173f65280170acdb840
ElRodrigote <lil.rorry@gmail.com>
2024-07-01 11:21:28 +0200

    styles: update Tooltip border and background styles

--- packages/ui/src/components/Tooltip/Tooltip.tsx
+++ packages/ui/src/components/Tooltip/Tooltip.tsx
@@ -43,8 +43,8 @@ const TooltipContent = forwardRef<
     ref={ref}
     sideOffset={sideOffset}
     className={twMerge(
-      "z-50 overflow-hidden rounded-12 p-3 text-sm shadow-3",
-      "text-text-med-em bg-outline-base-em",
+      "z-50 overflow-hidden rounded-12 p-3 text-sm shadow-3 border",
+      "text-text-med-em border-outline-base-em bg-surface-special-bg-neutral",
       "animate-in fade-in-0 zoom-in-95",
       "data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95",
       "data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
